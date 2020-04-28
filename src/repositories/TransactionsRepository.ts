a0cd75d81e622072cb08315bbd848acf2e6431d1
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2020-04-28 03:25:12 -0300

    Finalização desafio fundamentos node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    const { income, outcome } = this.transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          return {
+            ...acc,
+            income: acc.income + transaction.value,
+          };
+        }
+
+        return {
+          ...acc,
+          outcome: acc.outcome + transaction.value,
+        };
+      },
+      { ...balance },
+    );
+    balance.income = income;
+    balance.outcome = outcome;
+    balance.total = income - outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
